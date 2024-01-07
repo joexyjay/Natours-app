28df6d070761c2d2f8ec3cf1a803354424ad70c4
joexy <josephethethowo@gmail.com>
2024-01-07 12:25:50 +0100

    basic api requests using file system

--- app.ts
+++ app.ts
@@ -3,12 +3,16 @@ import express, {Request, Response} from 'express';
 
 const app = express()
 
+app.use(express.json())
+
 // app.get('/', (req:Request, res:Response)=> {
 //     res.status(200).json({
 //         message: 'Hello from the server side...'
 //     })
 // })
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`).toString())
+const tours = JSON
+.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+.toString())
 
 app.get('/api/v1/tours', (req:Request, res:Response) => {
     res.status(200).json({
@@ -17,6 +21,56 @@ app.get('/api/v1/tours', (req:Request, res:Response) => {
         data: {
             tours
         }
+    })
+})
+
+app.get('/api/v1/tours/:id', (req:Request, res:Response) => {
+    
+    const id = parseInt(req.params.id)
+    const tour = tours.find((el: { id: number; }) => el.id === id)
+
+    if(!tour){
+        return res.status(404).json({
+            status: "failed",
+            msg: "invalid ID"
+        })
+    }
+    res.status(200).json({
+        status:"success",
+        data: {
+            tour
+        }
+    })
+})
+
+app.post('/api/v1/tours', (req:Request, res:Response) => {
+    // console.log(req.body)
+    const newID = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({id:newID}, req.body)
+    tours.push(newTour)
+
+    fs.writeFile
+    (`${__dirname}/dev-data/data/tours-simple.json`, 
+    JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data: newTour
+        })
+    })
+})
+
+app.patch('/api/v1/tours/:id', (req:Request, res:Response) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            tours: "updated tour successfully"
+        }
+    })
+})
+
+app.delete('/api/v1/tours/:id', (req:Request, res:Response) => {
+    res.status(204).json({
+        data: null
     })
 })
 
--- dist/app.js
+++ dist/app.js
@@ -6,12 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 // app.get('/', (req:Request, res:Response)=> {
 //     res.status(200).json({
 //         message: 'Hello from the server side...'
 //     })
 // })
-const tours = JSON.parse(fs_1.default.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`).toString());
+const tours = JSON
+    .parse(fs_1.default.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+    .toString());
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: "success",
@@ -19,6 +22,47 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
+    });
+});
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const tour = tours.find((el) => el.id === id);
+    if (!tour) {
+        return res.status(404).json({
+            status: "failed",
+            msg: "invalid ID"
+        });
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour
+        }
+    });
+});
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
+    const newID = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newID }, req.body);
+    tours.push(newTour);
+    fs_1.default.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data: newTour
+        });
+    });
+});
+app.patch('/api/v1/tours/:id', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            tours: "updated tour successfully"
+        }
+    });
+});
+app.delete('/api/v1/tours/:id', (req, res) => {
+    res.status(204).json({
+        data: null
     });
 });
 const port = 3000;
