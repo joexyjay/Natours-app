6a36cafb4c38caa59719032c02f1ee3936e5662c
joexy <josephethethowo@gmail.com>
2024-01-10 11:29:11 +0100

    add more models

--- dist/models/tourModel.js
+++ dist/models/tourModel.js
@@ -8,13 +8,46 @@ const tourSchema = new mongoose_1.default.Schema({
     name: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: true
+    },
+    maxGroupSize: {
+        type: Number,
+        required: true
+    },
+    difficulty: {
+        type: String,
+        required: true
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
     },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
     price: Number,
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    imageCover: {
+        type: String,
+        required: true
+    },
+    images: [String],
+    startDates: [Date]
 }, {
     timestamps: true
 });
--- models/tourModel.ts
+++ models/tourModel.ts
@@ -2,21 +2,64 @@ import mongoose from "mongoose";
 
 export interface TourInstance extends mongoose.Document {
     name: string;
-    rating: number;
+    duration: number;
+    maxGroupSize: number;
+    difficulty: string;
+    ratingsAverage: number;
+    ratingsQuantity: number;
     price: number;
+    priceDiscount: number;
+    summary: string;
+    description: string;
+    imageCover: string;
+    images: [string];
+    startDates: [Date];
 }
 
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: true
+    },
+    maxGroupSize: {
+        type: Number,
+        required: true
+    },
+    difficulty: {
+        type: String,
+        required: true
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
     },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
     price: Number,
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    imageCover: {
+        type: String,
+        required: true
+    },
+    images: [String],
+    startDates: [Date]
 },
 {
     timestamps: true
